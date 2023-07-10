123495b090deda50cca71dafd8faa7df0db53e1b
jupinsimform <jupin@simformsolutions.com>
2023-07-10 18:39:29 +0530

    add carcard & cardetails component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 
 function CustomButton({
   btnType,
+  textStyles,
+  rightIcon,
   title,
   containerStyles,
   handleClick,
@@ -17,7 +19,17 @@ function CustomButton({
         handleClick;
       }}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 }
